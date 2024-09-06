9bb65373dea60a20b098dacbfc678b4740072bf8
taeju.Kwon <taeju.kwon@digipen.edu>
2024-09-07 01:16:13 +0900

    Elimentalism BasicSetting -1

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // 싱글톤을 할당할 전역 변수
+    public bool IsGameOver;
+    public GameObject GameOverUI;
+    public Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
