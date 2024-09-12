21986528236231045a892b5f5578dc95246c7975
CHEONHOPARK\pchh1 <pchh1353@gmail.com>
2024-09-12 12:46:57 +0900

    배경사진 추가

--- Assets/Script/Game/ThisCard.cs
+++ Assets/Script/Game/ThisCard.cs
@@ -15,13 +15,13 @@ public class ThisCard : MonoBehaviour
 
     void Start()
     {
-        thisCard[0] = CardDataBase.CardList[ThisID];
+        //thisCard[0] = CardDataBase.CardList[ThisID];
     }
 
     void Update()
     {
-        ID = thisCard[0].ID;
-        Attack = thisCard[0].Attacl;
+        //ID = thisCard[0].ID;
+        //Attack = thisCard[0].Attacl;
         
     }
 
