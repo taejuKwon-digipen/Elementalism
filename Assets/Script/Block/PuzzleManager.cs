979f42919f3270ed57b48cac20f47d68c68cd58e
SeongHyeon Lee <dltjdgus0606@naver.com>
2024-09-19 00:58:15 +0900

    퍼즐 랜덤생성(임시)

--- Assets/Script/Block/PuzzleManager.cs
+++ Assets/Script/Block/PuzzleManager.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BlockRoot[,] allTiles;
+
     void Start()
     {
-        
+        allTiles = new BlockRoot[width * 100, height * 100];
+        SetUp(); 
     }
 
-    // Update is called once per frame
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(100 * i + 450, 100 * j + 50);
+                GameObject background = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                background.transform.parent = this.transform;
+                background.name = "( " + i + " , " + j + ")";
+            }
+        }
+    }
+
     void Update()
     {
-        
     }
 }
