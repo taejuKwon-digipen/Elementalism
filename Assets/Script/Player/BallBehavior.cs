7be38c9269f43995f2d8fc9ebd8295049a5b4884
SeongHyeon Lee <dltjdgus0606@naver.com>
2024-11-11 19:24:53 +0900

    수정

--- Assets/Script/Player/BallBehavior.cs
+++ Assets/Script/Player/BallBehavior.cs
@@ -10,9 +10,7 @@ public class BallBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //_startPosition = transform.position;
         transform.position = player.transform.position;
-        //Debug.Log(_startPosition);
     }
 
     // Update is called once per frame
@@ -30,20 +28,8 @@ public class BallBehavior : MonoBehaviour
         var enemyPosition = focusedEnemy.GetComponent<RectTransform>().position;
         var entity = focusedEnemy.GetComponentInChildren<Entity>();
 
-        //this.transform.position = Vector3.zero + _startPosition;
-
-        //if (enemyPosition.x <= _startPosition.x ) {
-        //    entity.Hit(player, player.baseEntity.Type);
-        //    Destroy(this.gameObject);
-        //}
-
-        // 이동 로직 추가
-        //Vector3 direction = (enemyPosition - transform.position).normalized; // 방향 계산
-        //transform.position += direction * speed * Time.fixedDeltaTime; // 이동
-
         transform.position = Vector3.MoveTowards(transform.position, enemyPosition, speed * Time.fixedDeltaTime);
 
-        Debug.Log(enemyPosition);
         // 목표에 도착했는지 확인
         if (Vector3.Distance(transform.position, enemyPosition) <= 0.1f)
         {
