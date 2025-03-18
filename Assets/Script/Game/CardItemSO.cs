f18cf5836db08dd3ef07786ea57763c368d6914a
CHEONHOPARK\pchh1 <pchh1353@gmail.com>
2025-03-19 00:11:10 +0900

    인스펙터 수정

--- Assets/Script/Game/CardItemSO.cs
+++ Assets/Script/Game/CardItemSO.cs
@@ -4,9 +4,10 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI; // UI 관련 스크립트에 활용
+using Sirenix.OdinInspector;
 
 /*
-[System.Serializable] //unity �ν����� â���� ������ �� �ֵ��� ����
+[System.Serializable] //unityν â   ֵ 
 
 public class Prop
 {
@@ -20,24 +21,67 @@ public class Prop
 [System.Serializable]
 public class CardItem
 {
+    [TabGroup("기본")]
+    [LabelText("이름")]
     public string CardName;
+    
+    [TabGroup("기본")]
+    [LabelText("ID")]
     public int ID;
-    public int PowerLeft;
-    public int PowerRight;
+    
+    [TabGroup("기본")]
+    [TextArea(2, 3)]
+    [LabelText("설명")]
     public string CardDescription;
-    public bool UseMagic = false;
-    public float Percent;
+    
+    [TabGroup("기본")]
+    [LabelText("이미지")]
     public Texture2D cardImage;
-
-    public bool UseProp = false;
-    public bool UseBuff = false;
-    public bool UseDraw = false;
-
-    public bool UseImage = false;
-    public bool IsUnlocked = false;
-
-    public ShapeData cardShape; // 카드 모양 (ShapeData 사용)
+    
+    [TabGroup("능력치")]
+    [LabelText("기본 공격력")]
+    public int PowerLeft;
+    
+    [TabGroup("능력치")]
+    [LabelText("크리티컬 공격력")]
+    public int PowerRight;
+    
+    [TabGroup("능력치")]
+    [LabelText("원소")]
     public ElementType CreatedElementType;
+    
+    [TabGroup("특성")]
+    [LabelText("마법")]
+    public bool UseMagic = false;
+    
+    [TabGroup("특성")]
+    [LabelText("확률")]
+    [ShowIf("UseMagic")]
+    public float Percent;
+    
+    [TabGroup("특성")]
+    [LabelText("속성")]
+    public bool UseProp = false;
+    
+    [TabGroup("특성")]
+    [LabelText("버프")]
+    public bool UseBuff = false;
+    
+    [TabGroup("특성")]
+    [LabelText("드로우")]
+    public bool UseDraw = false;
+    
+    [TabGroup("설정")]
+    [LabelText("이미지사용")]
+    public bool UseImage = false;
+    
+    [TabGroup("설정")]
+    [LabelText("언락")]
+    public bool IsUnlocked = false;
+    
+    [TabGroup("설정")]
+    [LabelText("모양")]
+    public ShapeData cardShape;
 
     /* void Awake()
      {
@@ -54,6 +98,8 @@ public class CardItem
 [CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Object/ItemSO")]
 public class CardItemSO : ScriptableObject
 {
+    [TableList(ShowIndexLabels = true)]
+    [LabelText("카드 목록")]
     public CardItem[] items;
 
 #if UNITY_EDITOR
